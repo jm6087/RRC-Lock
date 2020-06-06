732c810f322f11b59e287c99721a733820d787fb
jm6087 <jm6087waze@gmail.com>
2020-06-06 06:37:27 -0500

    Update RRC-Lock.user.js

--- RRC-Lock.user.js
+++ RRC-Lock.user.js
@@ -10,11 +10,13 @@
 // ==/UserScript==
 
 function RRC-Locker () {
-//const SelFeat = W.selectionManager.getSelectedFeatures()[0]
-//const attLock = SelFeat.model.attributes.lockRank
+const SelFeat = W.selectionManager.getSelectedFeatures()[0]
+const attLock = SelFeat.model.attributes.lockRank
 
+Let RRCselected = attLock
+if (RRCselected <> 3){  
 document.querySelector("#edit-panel > div > div > div > div.tab-content > form > div > div > div > div > div.form-control.lock-level-selector.waze-radio-container > label:nth-child(12)").click()
-
+}
 //const mAction = new MultiAction();
 //let updates = {};
 //mAction.setModel(W.model);
