3395ed3f2394782a37ba518898cc93862fe8ad00
jm6087 <jm6087waze@gmail.com>
2020-06-06 17:58:47 -0500

    Update RRC-Lock.user.js

--- RRC-Lock.user.js
+++ RRC-Lock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME RRC Locker
 // @namespace    https://github.com/jm6087
-// @version      2020.05.28.00
+// @version      2020.06.06.00
 // @description  Locks RRCs
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
 // @exclude      https://www.waze.com/user/editor*
