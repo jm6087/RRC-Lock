0806436d95970996bb5484b97160552daf2630df
jm6087 <jm6087waze@gmail.com>
2020-06-06 20:38:14 -0500

    Update RRC-Lock.user.js

--- RRC-Lock.user.js
+++ RRC-Lock.user.js
@@ -4,10 +4,13 @@
 // @version      2020.06.06.00
 // @description  Locks RRCs
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
+// @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @exclude      https://www.waze.com/user/editor*
 // @author       jm6087 (with lots of help from SkiDooGuy)
 // @grant        none
 // ==/UserScript==
+
+WazeWrap.Events.register("selectionchanged", null, 'RRCLocker');
 
 function RRCLocker () {
 const SelFeat = W.selectionManager.getSelectedFeatures()[0]
@@ -29,8 +32,14 @@ function callbackFunc () {
 if (RRCselected <> 3){  
 document.querySelector("#edit-panel > div > div > div > div.tab-content > form > div > div > div > div > div.form-control.lock-level-selector.waze-radio-container > label:nth-child(12)").click()
 }
-  }
-//const mAction = new MultiAction();
-//let updates = {};
-//mAction.setModel(W.model);
-};
+
+function bootstrap(tries = 1) {
+	if (W && W.map &&
+		W.model && W.loginManager.user &&
+		$ ) {
+		init();
+	} else if (tries < 1000)
+		setTimeout(function () {bootstrap(tries++);}, 200);
+}
+}
+      bootstrap();
