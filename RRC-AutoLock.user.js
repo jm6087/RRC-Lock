9596525c3624b201def8e31546149a183049aa9e
jm6087 <jm6087waze@gmail.com>
2020-06-08 18:29:51 -0500

    Added WazeWrap pop up message
    And version number to tab

--- RRC-AutoLock.user.js
+++ RRC-AutoLock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME RRC AutoLock
 // @namespace    https://github.com/jm6087
-// @version      2020.06.08.03
+// @version      2020.06.08.05
 // @description  AutoLocks RRCs to set level instead of rank of editor
 // @author       jm6087 (with assistance from Dude495 and TheCre8r)
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -9,10 +9,13 @@
 // @grant        none
 // ==/UserScript==
 
+
+
 (function() {
     'use strict';
     var UPDATE_NOTES = `This should autolock RRCs to L4 upon selection of the RRC <br><br>
     Added tab as proof of concept for myself<br>
+    Added WazeWrap pop up when RRC selected<br>
     Script currently conflicts with WME Tiles Update.  Not allowing unverified RRCs to autolock initially
     This is my first script, hope it works and currently is very basic due to limited knoweledge.  Thanks for Dude495, TheCre8r, and SkiDooGuy for their assistance`
     var VERSION = GM_info.script.version;
@@ -31,8 +34,10 @@
         let RRCAutoLock4 = "#edit-panel > div > div > div > div.tab-content > form > div > div > div > div > div.form-control.lock-level-selector.waze-radio-container >" + RRCAutoLockLabel
         if (SelMan.hasSelectedFeatures() && SelModel.type === 'railroadCrossing' && SelModel.attributes.lockRank != 3){
             document.querySelector(RRCAutoLock4).click();
+            WazeWrap.Alerts.info(SCRIPT_NAME, ` RRC Lock level changed from lock level ${RRCAutolockRankplusOne}`);
             console.log(SCRIPT_NAME, "Version #", VERSION, "- Lock level changed from", RRCAutolockRankplusOne);
         }else{
+            WazeWrap.Alerts.info(SCRIPT_NAME, ` RRC lock not changed, already at lock level ${RRCAutolockRankplusOne}`);
             console.log (SCRIPT_NAME, "Version #", VERSION, "- RRC lock not changed, already at lock level", RRCAutolockRankplusOne);
         }
     }
@@ -41,6 +46,8 @@
         var $section = $("<div>");
         $section.html([
             '<div>',
+            '<h4 style="margin-bottom:0px;"><b>'+ SCRIPT_NAME +'</b></h4>',
+             VERSION +'</br></br>',
             '<h3>Hope to someday add option to choose your own lock level</h3></br>',
             '<h4>Currently the script automatically locks RRC at L4 when the RRC is selected</h4></br>',
             '<div>',
@@ -48,14 +55,33 @@
             '<p><b>Username: <span id="RRCAutoLockUsername"></span></p></b>',
             '<p><b>Rank: <span id="RRCAutoLockRank"></span></p></b>',
             '<p><b>Total edits: <span id="RRCAutoLockTotalEdits"></span></p></b>',
-            '<p><b>Total points: <span id="RRCAutoLockTotalPoints"></span></p></b>',
+    //        '<h3>Please select lock level <select id="RRCAutoLockLevelOption"></h3>',
+    //        '<option>3</option>',
+    //        '<option>4</option>',
+    //        '<option>5</option>',
+    //        '<option>6</option>',
+    //        '</select>',
+
+            //           '<p>Click the button to create a SELECT and an OPTION element.</p>',
+            //           '<button onclick="myFunction()">Try it</button>',
+
             '</div>',
             '</div>'
         ].join(' '));
 
         new WazeWrap.Interface.Tab('RRCAL', $section.html(), RRCAutoLockInitializeSettings);
     }
-
+//    function myFunction() {
+//       var x = document.createElement("SELECT");
+//        x.setAttribute("id", "mySelect");
+//        document.body.appendChild(x);
+//
+//        var z = document.createElement("option");
+//        z.setAttribute("value", "volvocar");
+//        var t = document.createTextNode("Volvo");
+//        z.appendChild(t);
+//        document.getElementById("mySelect").appendChild(z);
+//    }
     function RRCAutoLockInitializeSettings()
     {
         $('#RRCAutoLockUsername').text(W.loginManager.user.userName);
