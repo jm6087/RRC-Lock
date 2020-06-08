3e90ea9e1e607784c902e32446d8b1cb3b1652d2
jm6087 <jm6087waze@gmail.com>
2020-06-07 20:13:51 -0500

    Update RRC-AutoLock.user.js

--- RRC-AutoLock.user.js
+++ RRC-AutoLock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME RRC AutoLock
 // @namespace    https://github.com/jm6087
-// @version      2020.06.07.06
+// @version      2020.06.07.07
 // @description  AutoLocks RRCs to set level instead of rank of editor
 // @author       jm6087 (with assistance from Dude495 and TheCre8r)
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -12,6 +12,7 @@
 (function() {
     'use strict';
     var UPDATE_NOTES = `This should autolock RRCs to L4 upon selection of the RRC <br><br>
+    Added tab as proof of concept for myself<br>
     This is my first script, hope it works and currently is very basic due to limited knoweledge.  Thanks for Dude495, TheCre8r, and SkiDooGuy for their assistance`
     var VERSION = GM_info.script.version;
     var SCRIPT_NAME = GM_info.script.name;
@@ -31,8 +32,36 @@
             console.log (SCRIPT_NAME, "Version $", VERSION, "- RRC lock not changed, already at lock level", RRCAutolockRankplusOne);
         }
     }
+    function RRCAutoLockTab()
+    {
+        var $section = $("<div>");
+        $section.html([
+            '<div>',
+            '<h2>Hope to someday add option to choose your own lock level</h2>',
+            '<div>',
+            '<h3>User Info</h3></br>',
+            '<p><b>Username: <span id="RRCAutoLockUsername"></span></p></b>',
+            '<p><b>Rank: <span id="RRCAutoLockRank"></span></p></b>',
+            '<p><b>Total edits: <span id="RRCAutoLockTotalEdits"></span></p></b>',
+            '<p><b>Total points: <span id="RRCAutoLockTotalPoints"></span></p></b>',
+            '</div>',
+            '</div>'
+        ].join(' '));
+
+        new WazeWrap.Interface.Tab('RRCAL', $section.html(), RRCAutoLockInitializeSettings);
+    }
+
+    function RRCAutoLockInitializeSettings()
+    {
+        $('#RRCAutoLockUsername').text(W.loginManager.user.userName);
+        $('#RRCAutoLockRank').text(W.loginManager.user.rank + 1);
+        $('#RRCAutoLockTotalEdits').text(W.loginManager.user.totalEdits);
+        $('#RRCAutoLockTotalPoints').text(W.loginManager.user.totalPoints);
+        console.log("RRC AutoLock - Tab Created");
+    }
     function bootstrap(tries = 1) {
         if (W && W.map && W.model && W.loginManager.user && $ && WazeWrap.Ready ) {
+            RRCAutoLockTab()
             WazeWrap.Events.register("selectionchanged", null, setRRCAutoLock);
             WazeWrap.Interface.ShowScriptUpdate(SCRIPT_NAME, VERSION, UPDATE_NOTES);
             console.log(SCRIPT_NAME, "loaded");
