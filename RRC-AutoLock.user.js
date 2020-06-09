b6460f37dbe525f316ffb0a8358234fb0e168012
jm6087 <jm6087waze@gmail.com>
2020-06-08 21:09:31 -0500

    minor bug fixes

--- RRC-AutoLock.user.js
+++ RRC-AutoLock.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         WME RRC AutoLock
 // @namespace    https://github.com/jm6087
-// @version      2020.06.08.06
+// @version      2020.06.08.07
 // @description  AutoLocks RRCs to set level instead of rank of editor
-// @author       jm6087 (with assistance from Dude495 and TheCre8r)
+// @author       jm6087 (with assistance from Dude495, TheCre8r, and SkiDooGuy)
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
 // @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @grant        none
@@ -22,25 +22,34 @@
     This is my first script, hope it works and currently is very basic due to limited knoweledge.  Thanks for Dude495, TheCre8r, and SkiDooGuy for their assistance`
     var VERSION = GM_info.script.version;
     var SCRIPT_NAME = GM_info.script.name;
+    const USER_RANK = W.loginManager.user.rank + 1
+
     function setRRCAutoLock() {
+        let RRCAutolockRankplusOne;
         let SelMan = W.selectionManager;
-        let SelModel = SelMan.getSelectedFeatures()[0].model;
-        // Create an option variable using the following 2 let statements
-        let RRCAutoLockChildNumber = 12
-        let RRCAutoLockLabel = "label:nth-child(" + RRCAutoLockChildNumber+ ")"
-        if (SelModel.attributes.lockRank == null){
-            var RRCAutolockRankplusOne = ("AutoLock " + (SelModel.attributes.rank + 1));
-        }else{
-            var RRCAutolockRankplusOne = SelModel.attributes.lockRank + 1;
-        };
-        let RRCAutoLock4 = "#edit-panel > div > div > div > div.tab-content > form > div > div > div > div > div.form-control.lock-level-selector.waze-radio-container >" + RRCAutoLockLabel
-        if (SelMan.hasSelectedFeatures() && SelModel.type === 'railroadCrossing' && SelModel.attributes.lockRank != 3){
-            document.querySelector(RRCAutoLock4).click();
-            WazeWrap.Alerts.info(SCRIPT_NAME, ' RRC Lock level changed from lock level ' + RRCAutolockRankplusOne);
-            console.log(SCRIPT_NAME, "Version #", VERSION, "- Lock level changed from", RRCAutolockRankplusOne);
-        }else{
-            WazeWrap.Alerts.info(SCRIPT_NAME, ` RRC lock not changed, already at lock level ${RRCAutolockRankplusOne}`);
-            console.log (SCRIPT_NAME, "Version #", VERSION, "- RRC lock not changed, already at lock level", RRCAutolockRankplusOne);
+        if (SelMan.getSelectedFeatures().length > 0){
+            let SelModel = SelMan.getSelectedFeatures()[0].model;
+            // Create an option variable using the following 2 let statements
+            let RRCAutoLockChildNumber = 12
+            let RRCAutoLockLabel = "label:nth-child(" + RRCAutoLockChildNumber+ ")"
+            if (SelModel.attributes.lockRank == null){
+                RRCAutolockRankplusOne = ("AutoLock " + (SelModel.attributes.rank + 1));
+            }else{
+                RRCAutolockRankplusOne = SelModel.attributes.lockRank + 1;
+            };
+            let RRCAutoLock4 = "#edit-panel > div > div > div > div.tab-content > form > div > div > div > div > div.form-control.lock-level-selector.waze-radio-container >" + RRCAutoLockLabel
+            if (SelMan.hasSelectedFeatures() && SelModel.type === 'railroadCrossing' && SelModel.attributes.lockRank != 3){
+                if (USER_RANK <= RRCAutolockRankplusOne){
+                    document.querySelector(RRCAutoLock4).click();
+                    WazeWrap.Alerts.info(SCRIPT_NAME, ' RRC Lock level changed from lock level ' + RRCAutolockRankplusOne);
+                    console.log(SCRIPT_NAME, "Version #", VERSION, "- Lock level changed from", RRCAutolockRankplusOne);
+                }else{
+                    if (SelMan.hasSelectedFeatures() && SelModel.type === 'railroadCrossing' && SelModel.attributes.lockRank == 3){
+                        WazeWrap.Alerts.info(SCRIPT_NAME, ` RRC lock not changed, already at lock level ${RRCAutolockRankplusOne}`);
+                        console.log (SCRIPT_NAME, "Version #", VERSION, "- RRC lock not changed, already at lock level", RRCAutolockRankplusOne);
+                    }
+                }
+            }
         }
     }
     function RRCAutoLockTab()
@@ -49,7 +58,7 @@
         $section.html([
             '<div>',
             '<h4 style="margin-bottom:0px;"><b>'+ SCRIPT_NAME +'</b></h4>',
-             VERSION +'</br></br>',
+            VERSION +'</br></br>',
             '<h3>Hope to someday add option to choose your own lock level</h3></br>',
             '<h4>Currently the script automatically locks RRC at L4 when the RRC is selected</h4></br>',
             '<div>',
@@ -57,12 +66,12 @@
             '<p><b>Username: <span id="RRCAutoLockUsername"></span></p></b>',
             '<p><b>Rank: <span id="RRCAutoLockRank"></span></p></b>',
             '<p><b>Total edits: <span id="RRCAutoLockTotalEdits"></span></p></b>',
-    //        '<h3>Please select lock level <select id="RRCAutoLockLevelOption"></h3>',
-    //        '<option>3</option>',
-    //        '<option>4</option>',
-    //        '<option>5</option>',
-    //        '<option>6</option>',
-    //        '</select>',
+            //        '<h3>Please select lock level <select id="RRCAutoLockLevelOption"></h3>',
+            //        '<option>3</option>',
+            //        '<option>4</option>',
+            //        '<option>5</option>',
+            //        '<option>6</option>',
+            //        '</select>',
 
             //           '<p>Click the button to create a SELECT and an OPTION element.</p>',
             //           '<button onclick="myFunction()">Try it</button>',
@@ -73,17 +82,17 @@
 
         new WazeWrap.Interface.Tab('RRCAL', $section.html(), RRCAutoLockInitializeSettings);
     }
-//    function myFunction() {
-//       var x = document.createElement("SELECT");
-//        x.setAttribute("id", "mySelect");
-//        document.body.appendChild(x);
-//
-//        var z = document.createElement("option");
-//        z.setAttribute("value", "volvocar");
-//        var t = document.createTextNode("Volvo");
-//        z.appendChild(t);
-//        document.getElementById("mySelect").appendChild(z);
-//    }
+    //    function myFunction() {
+    //       var x = document.createElement("SELECT");
+    //        x.setAttribute("id", "mySelect");
+    //        document.body.appendChild(x);
+    //
+    //        var z = document.createElement("option");
+    //        z.setAttribute("value", "volvocar");
+    //        var t = document.createTextNode("Volvo");
+    //        z.appendChild(t);
+    //        document.getElementById("mySelect").appendChild(z);
+    //    }
     function RRCAutoLockInitializeSettings()
     {
         $('#RRCAutoLockUsername').text(W.loginManager.user.userName);
