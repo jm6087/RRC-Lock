c85b92fd4e72a2c51b9a1081d530cd99ea9e6c8e
jm6087 <jm6087waze@gmail.com>
2021-04-09 20:01:49 -0500

    Update RRC-AutoLock.user.js

--- RRC-AutoLock.user.js
+++ RRC-AutoLock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         WME RRC AutoLock - beta
+// @name         WME RRC AutoLock
 // @namespace    https://github.com/jm6087
-// @version      2021.04.09.02
+// @version      2021.04.10.00
 // @description  Locks RRCs and Cameras to set level instead of autolock to rank of editor
 // @author       jm6087 (with assistance from Dude495, TheCre8r, and SkiDooGuy)
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
